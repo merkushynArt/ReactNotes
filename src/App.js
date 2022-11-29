2461950a6fe4e2a2798b73a1bf9de540f5baac76
Artem <merkushynwrite@gmail.com>
2022-11-29 11:04:10 +0200

    starting learning the usestate hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+   const [name, setName] = useState('');
+
    return (
       <div className="App">
-         
+         <div className="wrapper__1">
+            <div className="input">
+               <label className='label'>Имя</label>
+               <input 
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+               />
+            </div>
+            <p>Привет {name}</p>
+         </div>
       </div>
    );
 }
